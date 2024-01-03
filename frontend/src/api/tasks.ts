f0897b4cda0ab7c0dbf44a0271a0fca73a55c47c
jennymar <jenmar528@gmail.com>
2024-01-03 09:57:34 -0800

    Implement part 1.2

--- frontend/src/api/tasks.ts
+++ frontend/src/api/tasks.ts
@@ -1,4 +1,4 @@
-import { type APIResult, get, handleAPIError, post } from "src/api/requests";
+import { type APIResult, get, handleAPIError, post, put } from "src/api/requests";
 
 /**
  * Defines the "shape" of a Task object (what fields are present and their types) for
@@ -103,6 +103,18 @@ export async function getAllTasks(): Promise<APIResult<Task[]>> {
       list.push(parseTask(task));
     }
     return { success: true, data: list };
+  } catch (error) {
+    return handleAPIError(error);
+  }
+}
+
+export async function updateTask(task: UpdateTaskRequest): Promise<APIResult<Task>> {
+  try {
+    // your code here
+    console.log("updateTask");
+    const response = await put("/api/task/", task);
+    const json = (await response.json()) as TaskJSON;
+    return { success: true, data: parseTask(json) };
   } catch (error) {
     return handleAPIError(error);
   }
